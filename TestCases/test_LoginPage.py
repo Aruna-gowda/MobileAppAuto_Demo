c60d4680d8edc1a8587ea8dff757f82ea2b39bda
Aruna-gowda <arunagowda984@gmail.com>
2024-09-19 13:28:01 +0530

    Update test_LoginPage.py
    ghjhjhj

--- TestCases/test_LoginPage.py
+++ TestCases/test_LoginPage.py
@@ -12,3 +12,25 @@
 #     @allure.title("Verify User can't login with Invalid Credentials")
 #     def test_TC002_InValidCredentials(self):
 #         LoginPage(self.driver).InValid_Login()
+import allure
+import pytest
+from allure_commons.types import AttachmentType
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+class TestHomePage():
+
+    @allure.title("Verify User able to login with valid Credentials")
+    def test_TC002_ValidCredentials(self):
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("detach", True)
+        options.add_argument( '--headless' )
+        options.add_argument("--disable-notifications")
+        options.add_argument( "--start-maximized" )
+        options.add_experimental_option('excludeSwitches', ['enable-logging'] )
+        driver = webdriver.Chrome(options=options)
+
+        driver.maximize_window()
+        driver.implicitly_wait(10)
+        driver.get("https://stage-app.edifyai.com/login")
+        driver.quit()
